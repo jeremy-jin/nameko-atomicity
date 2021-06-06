15b90c3537f103171b2e0b4518fd6c215fc08e1b
Jeremy <jeremy.jin@student.com>
2021-06-07 00:19:09 +0800

    add: Test

---
+++ test/test_dispatch_commands.py
@@ -0,0 +1,37 @@
+import pytest
+
+from nameko_atomicity import dispatch_after_commit
+from nameko_atomicity.dependency_base import CommandsWrapper
+
+
+class TestDispatchCommands:
+    def test_get_dependency(self, dispatch_commands, worker_ctx):
+        dependency = dispatch_commands.get_dependency(worker_ctx)
+        assert isinstance(dependency, CommandsWrapper)
+
+
+class TestDispatchAfterCommit:
+    def test_successful(self, worker_ctx, dispatch_commands, func):
+        def dispatch(*args, **kwargs):
+            pass
+
+        decorated_func = dispatch_after_commit(func)
+
+        dependency = dispatch_commands.get_dependency(worker_ctx)
+        setattr(dependency.container, "dispatch_commands", dependency)
+
+        dependency.append(func=dispatch, args="test_dispatch", kwargs={})
+        func.return_value = "success"
+        return_value = decorated_func(dependency.container)
+
+        assert func.called
+        assert return_value == "success"
+
+    def test_once_failed(self, worker_ctx, dispatch_commands, func):
+        decorated_func = dispatch_after_commit(func)
+        dependency = dispatch_commands.get_dependency(worker_ctx)
+        setattr(dependency.container, "dispatch_commands", dependency)
+        func.side_effect = Exception("Boom!")
+        pytest.raises(Exception, decorated_func, dependency.container)
+
+        assert func.called
---
+++ test/test_rollback_coommands.py
@@ -0,0 +1,40 @@
+import pytest
+from mock import Mock
+
+from nameko_atomicity import rollback_once_failed
+from nameko_atomicity.dependency_base import CommandsWrapper
+
+
+class TestRollbackCommands:
+    def test_get_dependency(self, rollback_commands, container):
+        worker_ctx = Mock()
+        worker_ctx.container = container
+        dependency = rollback_commands.get_dependency(worker_ctx)
+
+        assert isinstance(dependency, CommandsWrapper)
+
+
+class TestRollbackOnceFailed:
+    def test_successful(self, worker_ctx, rollback_commands, func):
+        decorated_func = rollback_once_failed(func)
+        dependency = rollback_commands.get_dependency(worker_ctx)
+        setattr(dependency.container, "rollback_commands", dependency)
+
+        func.return_value = "success"
+        return_value = decorated_func(dependency.container)
+
+        assert func.called
+        assert return_value == "success"
+
+    def test_once_failed(self, worker_ctx, rollback_commands, func):
+        decorated_func = rollback_once_failed(func)
+        func.side_effect = Exception("Boom!")
+        rollback_command = Mock()
+        dependency = rollback_commands.get_dependency(worker_ctx)
+        setattr(dependency.container, "rollback_commands", dependency)
+        dependency.append(func=rollback_command, args="test_dispatch", kwargs={})
+
+        pytest.raises(Exception, decorated_func, dependency.container)
+
+        assert func.called
+        assert rollback_command.called
