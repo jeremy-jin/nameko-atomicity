c0b36da89b2b40d11e30d11de8bc177927a01ab2
Jeremy <jeremy.jin@student.com>
2021-10-15 17:24:02 +0800

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nameko-atomicity',
-    version='0.0.2',
+    version='1.0.0',
     description='Atomicity dependency for nameko services',
     author='jeremy-jin',
     author_email='306795597@qq.com',
